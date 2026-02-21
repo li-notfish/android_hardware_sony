b1e61052db109cb44044117993a9feb8af2ab6a2
onlyfish <linotfish@outlook.com>
2026-02-21 17:04:18 +0800

    sepolicy: fix app_system rw battery supply

--- packages/Charger/XperiaCharger/sepolicy/SEPolicy.mk
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2025 XperiaLabs Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-BOARD_VENDOR_SEPOLICY_DIRS += hardware/sony/XperiaModules/Charger/XperiaCharger/sepolicy/vendor
